20b3db1cd732fdcc10e8852c09ef0e0f4e10ba17
Samguzman30 <19samuelguzman@gmail.com>
2022-04-24 20:11:43 -0300

    cambios

---
+++ area_rectangulo.php
@@ -0,0 +1,8 @@
+<?php
+//definicion
+function calcularAreaRect($base, $altura){
+    return $base * $altura;
+} 
+echo "el area es " . calcularAreaRect (100, 0.60) . "<br>";
+echo "el area es " . calcularAreaRect(800, 300);
+?>
