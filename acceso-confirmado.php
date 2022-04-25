8b6c7cd16eabdff65e75311a49edf934f061e180
Samguzman30 <19samuelguzman@gmail.com>
2022-04-25 01:03:27 -0300

    cambios

---
+++ acceso-confirmado.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso confirmado</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center mb-4">
+                <h1>Bienvenid@ al sistema</h1>
+            </div>
+        </div>
+        <div class="alert alert-primary" role="alert">
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor molestias atque nesciunt modi consectetur quis nostrum, tempore cum doloribus, quam necessitatibus assumenda enim non eius incidunt, impedit eligendi. Soluta, amet.</p>
+</div>
+<div class="row">
+    <div class="col-12 text-end">
+        <a href="index.php"> <button type="submit" class="btn btn-primary">Salir</button></a>
+
+    </div>
+
+</div>
+
+    </main>
+    
+</body>
+</html>
